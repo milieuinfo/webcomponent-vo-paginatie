11473c77b7d757971f584c75f0d5692dd0f3dbe7
Tom Coemans <tom.coemans@gmail.com>
2019-01-14 18:50:16 +0100

    GVR-189 ga naar eerste en laatste pagina functionaliteit toegevoegd

--- vo-paginatie.js
+++ vo-paginatie.js
@@ -10,16 +10,21 @@ class VoPaginatie extends LitElement {
     
     static get properties() {
         return {
-            'huidige-pagina': Number,
+            'huidige-pagina': {
+                type: Number,
+                value: 0
+            },
             'totaal-aantal-paginas': Number
         }
     }
 
     render() {
         return html`
-            <button id="vorige" @click="${this._vorige}">&lt;</button> 
-            <span>${this['huidige-pagina']}/${this['totaal-aantal-paginas']}<span>  
-            <button id="volgende" @click="${this._volgende}">&gt;</button>
+            <button id="vorige" @click="${this._eerstePagina}" ?disabled="${this._isEerstePagina()}">&lt;&lt;</button>
+            <button id="vorige" @click="${this._vorige}" ?disabled="${this._isEerstePagina()}">&lt;</button>
+            <span>${this['huidige-pagina']}/${this['totaal-aantal-paginas']}<span>
+            <button id="volgende" @click="${this._volgende}" ?disabled="${this._isLaatstePagina()}">&gt;</button>
+            <button id="volgende" @click="${this._laatstePagina}" ?disabled="${this._isLaatstePagina()}">&gt;&gt;</button>
         `;
     }
 
@@ -29,6 +34,26 @@ class VoPaginatie extends LitElement {
 
     _volgende() {
         this['huidige-pagina']++;
+    }
+
+    _eerstePagina() {
+        this._setHuidigePagina(0);
+    }
+
+    _laatstePagina() {
+        this._setHuidigePagina(this['totaal-aantal-paginas']);
+    }
+
+    _isEerstePagina() {
+        return this['huidige-pagina'] == 0;
+    }
+
+    _isLaatstePagina() {
+        return this['huidige-pagina'] == this['totaal-aantal-paginas'];
+    }
+
+    _setHuidigePagina(value) {
+        this['huidige-pagina'] = value;
     }
 }
 
