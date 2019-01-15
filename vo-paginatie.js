cd8435aeea5dc0167401a08e3596c4bbc3da59e4
wautergu <guy.wauters@vlaanderen.be>
2019-01-15 09:40:26 +0100

    GVR-179: enable/disable previous button

--- vo-paginatie.js
+++ vo-paginatie.js
@@ -1,4 +1,4 @@
-import {LitElement, html} from '../../node_modules/vodomg-litelement/@polymer/lit-element/lit-element.js';
+import {LitElement, html} from "../../node_modules/@polymer/lit-element/lit-element"
 
 class VoPaginatie extends LitElement {
     static get EVENTS() {
@@ -12,25 +12,34 @@ class VoPaginatie extends LitElement {
         return {
             huidigePagina: {
                 type: Number,
-                attribute: 'huidige-pagina'
+                attribute: 'huidige-pagina',
+                hasChanged(newValue, oldValue) {
+                    return true;
+                }
             },
             totaalAantalPaginas: {
                 type: Number,
                 attribute: 'totaal-aantal-paginas'
-            }
+            },
+            elementsOnPage: Number,
+            totalElements: Number,
+            _eerstePagina: Boolean
         }
     }
 
     render() {
         return html`
-            button id="vorige" @click="${this._vorige}">&lt;</button> 
+            <button id="vorige" @click="${this._vorige}" ?disabled="${this._isEerstePagina()}" >&lt;</button> 
             <span>${this.huidigePagina}/${this.totaalAantalPaginas}<span>  
             <button id="volgende" @click="${this._volgende}">&gt;</button>
         `;
     }
 
+
     _vorige () {
-        this.huidigePagina--;
+        if (this.huidigePagina >= 0) {
+            this.huidigePagina--;
+        }
     }
 
     _volgende() {
@@ -38,6 +47,7 @@ class VoPaginatie extends LitElement {
     }
 
     _isEerstePagina() {
+        console.log("check eerste pg");
         return this.huidigePagina == 0;
     }
 
@@ -45,5 +55,6 @@ class VoPaginatie extends LitElement {
         return this.huidigePagina == this.totaalAantalPaginas;
     }
 }
+
 
 customElements.define('vo-paginatie', VoPaginatie);
