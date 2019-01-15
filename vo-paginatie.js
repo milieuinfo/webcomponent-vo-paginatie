c24dd394df0a810b7e1e2ab4f1239d253f7fc115
Tom Coemans <tom.coemans@gmail.com>
2019-01-15 11:19:32 +0100

    GVR-189 test toegevoegd huidige pagina en totaal aantal paginas

--- vo-paginatie.js
+++ vo-paginatie.js
@@ -27,7 +27,7 @@ class VoPaginatie extends LitElement {
         return html`
             <button id="eerste" @click="${this._eerstePagina}" ?disabled="${this._isEerstePagina()}">&lt;&lt;</button>
             <button id="vorige" @click="${this._vorige}" ?disabled="${this._isEerstePagina()}">&lt;</button>
-            <span>${this.huidigePagina}/${this.totaalAantalPaginas}<span>
+            <span id="huidige_pagina">${this.huidigePagina}</span>/<span id="totaal_aantal_paginas">${this.totaalAantalPaginas}</span>
             <button id="volgende" @click="${this._volgende}" ?disabled="${this._isLaatstePagina()}">&gt;</button>
             <button id="laatste" @click="${this._laatstePagina}" ?disabled="${this._isLaatstePagina()}">&gt;&gt;</button>
         `;
@@ -67,14 +67,6 @@ class VoPaginatie extends LitElement {
 
     _laatstePagina() {
         this._setHuidigePagina(this.totaalAantalPaginas);
-    }
-
-    _isEerstePagina() {
-        return this.huidigePagina == 0;
-    }
-
-    _isLaatstePagina() {
-        return this.huidigePagina == this.totaalAantalPaginas;
     }
 
     _setHuidigePagina(value) {
